0ec770f3d073b0791b49b7568fe6d44dd4e1d123
Josh Santos <josh@santos.cloud>
2024-09-04 19:28:06 +0700

    Add initial tree structure for Kenji

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,6 +1,38 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 import { PageProps } from '@/types';
+import CenteredTree from '../Components/CenteredTree';
+
+// This is a simplified example of an org chart with a depth of 2.
+// Note how deeper levels are defined recursively via the `children` property.
+const sampleInquiryChart = {
+  name: 'Inquiry',
+  attributes: {
+    content: 'What is the colour of the sky?',
+  },
+  children: [
+    {
+      name: 'Inquiry',
+      attributes: {
+        content: "The sky itself, or our perception of it?",
+      },
+      children: [
+        {
+          name: 'Response',
+          attributes: {
+            content: 'The sky itself.',
+          },
+        },
+      ],
+    },
+    {
+      name: 'Response',
+      attributes: {
+          content: 'The sky is clear.'
+      },
+    },
+  ],
+};
 
 export default function Dashboard({ auth }: PageProps) {
     return (
@@ -14,6 +46,9 @@ export default function Dashboard({ auth }: PageProps) {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
+                    </div>
+                    <div id="treeWrapper">
+                        <CenteredTree data={sampleInquiryChart} />
                     </div>
                 </div>
             </div>
