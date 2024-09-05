3c08c64c485a60fab0881bd67917d5e0758c3489
Josh Santos <josh@santos.cloud>
2024-09-05 17:23:33 +0700

    Add cyclic graph - dragging not working

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,17 +1,19 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 import { PageProps } from '@/types';
-import CenteredTree from '../Components/CenteredTree';
+import CenteredTree from '../Components/Trees/DAG';
+import Tree from '../Components/Trees/WebCola';
 
 // This is a simplified example of an org chart with a depth of 2.
 // Note how deeper levels are defined recursively via the `children` property.
-const sampleInquiryChart = {
+const sampleInquiryChart = [{
   name: 'Inquiry',
   attributes: {
     content: 'What is the colour of the sky?',
   },
   children: [
     {
+      id: '2',
       name: 'Inquiry',
       attributes: {
         content: "The sky itself, or our perception of it?",
@@ -26,13 +28,60 @@ const sampleInquiryChart = {
       ],
     },
     {
-      name: 'Response',
+      id: '2',
+      name: 'Inquiry',
       attributes: {
-          content: 'The sky is clear.'
+        content: "The sky itself, or our perception of it?",
       },
+      children: [
+        {
+          name: 'Response',
+          attributes: {
+            content: 'The sky itself.',
+          },
+        },
+      ],
     },
-  ],
-};
+  ]
+},
+{
+    name: 'Synthesis',
+    parent: ['1', '2'],
+    children: [
+        {
+            name: 'Response',
+            attributes: {
+                content: 'The sky is clear.'
+            },
+        },
+
+    ]
+}]
+
+// const nodes = [
+//   { id: 0, name: 'Root', parent: null },
+//   { id: 1, name: 'Child 1', parent: 0 },
+//   { id: 2, name: 'Child 2', parent: 0 },
+//   // ... additional nodes
+// ];
+// const links = nodes.filter(d => d.parent !== null).map(d => ({ source: d.parent, target: d.id }));
+
+const example = {
+    "nodes":[
+      {"name":"a","width":60,"height":40},
+      {"name":"b","width":60,"height":40},
+      {"name":"c","width":60,"height":40},
+      {"name":"d","width":60,"height":40},
+      {"name":"e","width":60,"height":40}
+    ],
+    "links":[
+      {"source":0,"target":1},
+      {"source":1,"target":2},
+      {"source":2,"target":0},
+      {"source":2,"target":3}
+    ]
+}
+
 
 export default function Dashboard({ auth }: PageProps) {
     return (
@@ -48,7 +97,7 @@ export default function Dashboard({ auth }: PageProps) {
                         <div className="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
                     </div>
                     <div id="treeWrapper">
-                        <CenteredTree data={sampleInquiryChart} />
+                        <Tree nodes={example.nodes} links={example.links} />
                     </div>
                 </div>
             </div>
