d242928834e423cac1b9f136312d9f08c7345224
Your SW <admin@kusala.tech>
2025-10-15 05:38:32 +0700

    Add initial scaffold for new app

---
+++ lsa/DhammaLens/main.go
@@ -0,0 +1,20 @@
+// Copyright (C) 2568 Josh/Kusala Tech (Mindful Sandbox)
+// Licensed under the GNU Affero General Public License v3.0 or later (AGPLv3).
+
+// What is it?
+/*
+   A tool to assist with Dhamma talk authorship from a source language to one or many destination language(s).
+*/
+
+// Why?
+/*
+   To help Dhamma teachers to share what they teach via various platforms.
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+        fmt.Println("Scaffold")
+}
